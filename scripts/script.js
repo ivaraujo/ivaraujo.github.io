c761f6648062bbc79c9a919c92244ebd230b5522
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2023-09-04 12:22:08 -0300

    Active btn topo

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,13 @@ btnTop.addEventListener("click", function(){
     window.scrollTo(0,0);
 });
 
+document.addEventListener("scroll", ocultar);
+
 function ocultar(){
-    if(window.scrollY > 10){
+    if(window.scrollY > 150){
         btnTop.style.display = "flex";
     }
     else{
         btnTop.style.display = "none";
     }
 }
-ocultar();
